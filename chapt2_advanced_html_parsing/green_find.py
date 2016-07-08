e7858c61f92339555872365d1a1820ba3faffe96
Kevin Carr <cochewen@Kevins-MacBook-Air.local>
2016-07-08 12:12:22 -0400

    added examples

--- chapt2_advanced_html_parsing/green_find.py
+++ chapt2_advanced_html_parsing/green_find.py
@@ -6,3 +6,11 @@ bsObj = BeautifulSoup(html)
 nameList = bsObj.findAll("span", {"class":"green"})
 for name in nameList:
 	print(name.get_text())
+
+
+
+nameList = bsObj.findAll(text="the prince")
+print(len(nameList))
+
+allText = bsObj.findAll(id="text")
+print(allText[0].get_text)
